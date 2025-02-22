8dcabc6958a0e02cd9a709b8989805e0bed617cc
Alpyg <alpyg@pm.me>
2025-02-22 01:40:25 -0500

    Added recipe title

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -55,6 +55,8 @@ export default async function Page({
 
   return (
     <main className="space-y-4">
+      <h1 className="text-center text-xl font-semibold">{frontmatter.title}</h1>
+
       {frontmatter.video && (
         <Video src={frontmatter.video} />
       )}
